cbb892665ce839d8abe2b66be2c03a877e906080
mnsdzwd <54425283+mnsdzwd@users.noreply.github.com>
2023-04-03 05:01:04 +0800

    Update 一个简单的抓取.py

--- 一个简单的抓取.py
+++ 一个简单的抓取.py
@@ -3,20 +3,20 @@
 Spyder Editor
 
 """
-
 import requests
 from bs4 import BeautifulSoup
+import jieba
 
-url = 'https://www.163.com/'
+url = 'https://news.163.com/'
 
-
+#伪装成浏览器
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+#取得网页的内容
 response = requests.get(url, headers=headers)
-
-
 #print(response)
 soup = BeautifulSoup(response.text, 'html.parser')
-#print(soup)
-#for link in soup.find_all('a'):
-#    print(link.get('href'))
+print(soup)
+#调取网址
+for link in soup.find_all('a'):
+    print(link.get('href'))
